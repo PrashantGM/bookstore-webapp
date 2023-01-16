606d74429b6d05404ce72ec78b95ac8728eaa887
PrashantGM <gmprem14@gmail.com>
2023-01-16 10:57:18 +0545

    added confirm password field &  validation in user registration

--- views/js/register.js
+++ views/js/register.js
@@ -2,12 +2,24 @@ const navDropdown = document.querySelector('#dropdown-profile');
 navDropdown.style.display = 'none';
 const registerForm = document.getElementById('form-register');
 const btnRegister = document.getElementById('btn-register');
-btnRegister.addEventListener('click', (e) => {
+const password = document.querySelector('input[name="password"]');
+const confirmPassword = document.querySelector(
+  'input[name="confirm-password"]'
+);
+confirmPassword.addEventListener('input', function () {
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords doesn't match");
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+});
+
+registerForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
   const username = document.querySelector('input[name="username"]').value;
   const email = document.querySelector('input[name="email"]').value;
-  const password = document.querySelector('input[name="password"]').value;
+  registerForm.checkValidity();
   const data = { username, email, password };
 
   fetch('http://localhost:8000/user/register', {
