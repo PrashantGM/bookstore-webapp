7f235b3b53e2d9e0802dc26db993ca9f2872e42e
PrashantGM <gmprem14@gmail.com>
2023-01-18 13:02:37 +0545

    implemented user specific nav menu display, fixed input quantity bug and did refactoring for better code

--- views/js/book.js
+++ views/js/book.js
@@ -1,16 +1,65 @@
 import { toast } from './toast.js';
 
-const similarSection = document.querySelector('.others');
-let currentGenre = similarSection.getAttribute('data-genre');
+let loggedIn = false;
+let parsedUserData;
 let bookId = 0;
-fetch(`http://localhost:8000/books/similar?genre=${currentGenre}`, {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  method: 'GET',
-})
-  .then((res) => res.json())
-  .then((result) => {
+async function onload() {
+  await loadNav();
+  await loadPage();
+  await addtoCart();
+}
+onload();
+
+async function loadNav() {
+  const btnProfile = document.querySelector('.btn-profile');
+  console.log('btnprofile', btnProfile);
+
+  try {
+    const isLoggedIn = await viewLoggedIn();
+    loggedIn = isLoggedIn.success;
+    if (loggedIn) {
+      parsedUserData = JSON.parse(isLoggedIn.payload);
+
+      btnProfile.textContent = parsedUserData.username;
+      document.querySelector('.li-home').style.display = 'none';
+      document.querySelector('.li-genre').style.display = 'none';
+      document.querySelector('.li-about').style.display = 'none';
+
+      btnProfile.textContent = parsedUserData.username;
+      const userIcon = document.createElement('i');
+      userIcon.className = 'fa fa-user-circle';
+      btnProfile.appendChild(userIcon);
+
+      const downIcon = document.createElement('i');
+      downIcon.className = 'fa fa-caret-down';
+      btnProfile.appendChild(downIcon);
+
+      document.querySelector('#navRead').style.display = 'none';
+      document.querySelector('#nav-dashboard').style.display = 'none';
+    } else {
+      btnProfile.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = 'http://localhost:8000/user/login';
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function loadPage() {
+  const similarSection = document.querySelector('.others');
+  let currentGenre = similarSection.getAttribute('data-genre');
+  try {
+    const response = await fetch(
+      `http://localhost:8000/books/similar?genre=${currentGenre}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'GET',
+      }
+    );
+    const result = await response.json();
     const books = result.data;
     books.forEach((book) => {
       bookId = book.id;
@@ -53,54 +102,54 @@ fetch(`http://localhost:8000/books/similar?genre=${currentGenre}`, {
         window.location.assign(`http://localhost:8000/books/${book.id}`);
       });
     });
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error);
+  }
+}
+
+async function addtoCart() {
+  const totalPrice = document.querySelector('#price-total');
+  let price = Number(totalPrice.getAttribute('data-price'));
+  let totalAmount = price * 1;
+  const inputQuantity = document.querySelector('input[name="quantity"]');
+  let quantity;
+  inputQuantity.addEventListener('input', (e) => {
+    e.preventDefault();
+    quantity = Number(inputQuantity.value);
+    totalAmount = price * quantity;
+    totalPrice.innerHTML = `Rs ${totalAmount}`;
   });
 
-const totalPrice = document.querySelector('#price-total');
-let price = Number(totalPrice.getAttribute('data-price'));
-let totalAmount = price * 1;
-const inputQuantity = document.querySelector('input[name="quantity"]');
-let quantity = Number(inputQuantity.value);
-
-inputQuantity.addEventListener('input', (e) => {
-  e.preventDefault();
-  totalAmount = price * quantity;
-  console.log('het');
-  console.log(totalAmount);
-  totalPrice.innerHTML = `Rs ${totalAmount}`;
-});
-
-const btnAddCart = document.querySelector('#btn-addCart');
-btnAddCart.addEventListener('click', async (e) => {
-  e.preventDefault();
-  console.log(totalAmount);
-  const isLoggedIn = await viewLoggedIn();
-  if (isLoggedIn.success) {
-    const parsedUserData = JSON.parse(isLoggedIn.payload);
-    const userId = parsedUserData.id;
-    const response = await fetch(`http://localhost:8000/order/${userId}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({ quantity, totalAmount, bookId }),
-    });
-    const parsedResponse = await response.json();
-    console.log(parsedResponse);
-    const bookSection = document.querySelector('.book-section');
-    toast.initToast(bookSection);
-    toast.generateToast({
-      message: parsedResponse.msg,
-      background: '#eaf7fb',
-      color: 'green',
-      length: '2000ms',
-    });
-  } else {
-    window.location.replace('http://localhost:8000/user/login');
-  }
-});
+  const btnAddCart = document.querySelector('#btn-addCart');
+  btnAddCart.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+      if (loggedIn) {
+        const userId = parsedUserData.id;
+        const response = await fetch(`http://localhost:8000/order/${userId}`, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({ quantity, totalAmount, bookId }),
+        });
+        const parsedResponse = await response.json();
+        const bookSection = document.querySelector('.book-section');
+        toast.initToast(bookSection);
+        toast.generateToast({
+          message: parsedResponse.msg,
+          background: '#eaf7fb',
+          color: 'green',
+          length: '2000ms',
+        });
+      } else {
+        window.location.replace('http://localhost:8000/user/login');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
 
 async function viewLoggedIn() {
   try {
